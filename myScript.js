3a557588aaf9660d9ce8599fb4c5256bd0d68ff6
sebberise <95592124+sebberise@users.noreply.github.com>
2022-01-31 15:06:48 +0100

    Add files via upload

---
+++ myScript.js
@@ -0,0 +1,19 @@
+const slideshowImages = document.querySelectorAll(".popup-inner img");
+
+const nextImageDelay = 7000;
+let currentImageCounter = 0; // setting a variable to keep track of the current image (slide)
+
+// slideshowImages[currentImageCounter].style.display = "block";
+slideshowImages[currentImageCounter].style.opacity = 1;
+
+setInterval(nextImage, nextImageDelay);
+
+function nextImage() {
+  // slideshowImages[currentImageCounter].style.display = "none";
+  slideshowImages[currentImageCounter].style.opacity = 0;
+
+  currentImageCounter = (currentImageCounter+1) % slideshowImages.length;
+
+  // slideshowImages[currentImageCounter].style.display = "block";
+  slideshowImages[currentImageCounter].style.opacity = 1;
+}
