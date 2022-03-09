a679f3c029180fb9c208d63fb5e50d7481c6f797
sebberise <95592124+sebberise@users.noreply.github.com>
2022-03-09 12:45:02 +0100

    Update luft.js

--- luft.js
+++ luft.js
@@ -1,16 +1,1 @@
-const slideshowImages = document.querySelectorAll(".popup-inner img");
 
-const nextImageDelay = 7000;
-let currentImageCounter = 0; 
-
-slideshowImages[currentImageCounter].style.opacity = 1;
-
-setInterval(nextImage, nextImageDelay);
-
-function nextImage() {
-  slideshowImages[currentImageCounter].style.opacity = 0;
-
-  currentImageCounter = (currentImageCounter+1) % slideshowImages.length;
-
-  slideshowImages[currentImageCounter].style.opacity = 1;
-}
